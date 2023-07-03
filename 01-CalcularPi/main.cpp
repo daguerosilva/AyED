4250b46f52d36631dd3c6a44e0623f3ccfeee5ac
Daguerosilva <daguerosilva@frba.utn.edu.ar>
2023-07-03 11:02:31 -0300

    main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main(){
 
-    double numerador {1};
-    double denominador {1}; 
     double pi {0};
-    int signo {1};
+    const double numerador {1};
+    double denominador {1}; 
+    short int signo {1};
     
     for (double i = 0; i <= (pi = 3.141592); ++i) {
     pi += signo * (numerador / denominador);
     denominador += 2; 
     signo *= -1;
-    }
+	}
 
     cout << setprecision(7) << pi << endl;
 
